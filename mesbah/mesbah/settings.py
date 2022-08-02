16bc933de54746f6fa282218580db61f65bfc306
Arman Hadi <39586567+Arman-Hadi@users.noreply.github.com>
2022-08-02 16:30:48 +0430

    allow hosts

--- mesbah/mesbah/settings.py
+++ mesbah/mesbah/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-#59cvi^butkkrr$qq##*wp2#^l9&u^@v*v^yjg4ft#bq%t=&tg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
