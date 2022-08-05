019fabfe6d44e92a9d1fa7f4480536e8a31d1731
Arman Hadi <39586567+Arman-Hadi@users.noreply.github.com>
2022-08-05 18:30:05 +0430

    'locals'

--- mesbah/mesbah/settings.py
+++ mesbah/mesbah/settings.py
@@ -132,3 +132,9 @@ STATIC_ROOT = BASE_DIR / 'static'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+try:
+    from .local_settings import *
+except:
+    print('no locals')
