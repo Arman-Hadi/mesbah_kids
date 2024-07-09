a1ef62a55a83a34046c4b146f2c937216114b463
Arman-Hadi <armanhadi728@yahoo.com>
2024-07-09 15:15:44 +0330

    name

--- mesbah/api/serializers.py
+++ mesbah/api/serializers.py
@@ -4,6 +4,11 @@ from core.models import Kid
 
 
 class KidSerializer(serializers.ModelSerializer):
+    name = serializers.SerializerMethodField()
+
     class Meta:
         model = Kid
         fields = '__all__'
+
+    def get_name(self, obj):
+        return f"{obj.first_name} {obj.last_name}"
--- mesbah/api2/serializers.py
+++ mesbah/api2/serializers.py
@@ -4,6 +4,8 @@ from core.models import Kid
 
 
 class KidSerializer(serializers.ModelSerializer):
+    name = serializers.SerializerMethodField()
+
     class Meta:
         model = Kid
         fields = '__all__'
@@ -11,3 +13,6 @@ class KidSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         kid = Kid.objects.create(**validated_data)
         return kid
+
+    def get_name(self, obj):
+        return f"{obj.first_name} {obj.last_name}"
