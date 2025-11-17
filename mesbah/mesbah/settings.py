fb6daf9648dc6d702b744b705a3bb889cf6e79a7
Arman-Hadi <armanhadi728@yahoo.com>
2025-11-17 16:04:21 +0330

    update settings.

--- mesbah/mesbah/settings.py
+++ mesbah/mesbah/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,13 +23,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#59cvi^butkkrr$qq##*wp2#^l9&u^@v*v^yjg4ft#bq%t=&tg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.getenv('DEBUG', 1))
 
 ALLOWED_HOSTS = ["*"]
 
 CSRF_TRUSTED_ORIGINS = [
     'https://*.mesbahkids.ir', 'http://*.mesbahkids.ir', 'http://mesbahkids.ir', 'http://mesbahkids.ir',
-    'https://javaaneha.ir', 'https://api.javaaneha.ir'
+    'https://javaaneha.ir', 'https://api.javaaneha.ir',
+    'https://*.bmna.ir', 'http://*.bmna.ir', 'http://bmna.ir', 'https://bmna.ir'
 ]
 
 # Application definition
@@ -131,10 +132,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = '/var/www/mesbah/static'
+STATIC_ROOT = os.getenv('STATIC_ROOT', BASE_DIR / 'static')
 
 MEDIA_URL = 'media/'
-MEDIA_ROOT = '/var/www/mesbah/media'
+MEDIA_ROOT = os.getenv('MEDIA_ROOT', BASE_DIR / 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
