080db8c35883e842cc02a12dd8dfb478dc50ee93
Arman-Hadi <armanhadi728@yahoo.com>
2025-11-17 17:21:56 +0330

    just load static when no debug

--- mesbah/mesbah/urls.py
+++ mesbah/mesbah/urls.py
@@ -22,4 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('api2/', include('api2.urls')),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
